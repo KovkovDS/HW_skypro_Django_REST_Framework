7b12a81106f9a04681674c365b9a6029f49304b9
Kovkov-D-S <158774590+Kovkov-D-S@users.noreply.github.com>
2025-03-08 01:45:49 +0300

    added payment history output for user profile

--- users/serializer.py
+++ users/serializer.py
@@ -2,15 +2,19 @@ from rest_framework import serializers
 from users.models import User, Payments
 
 
+class PaymentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Payments
+        fields = '__all__'
+
+
 class ProfileSerializer(serializers.ModelSerializer):
+    payment_history = PaymentSerializer(source='payments_set', many=True)
 
     class Meta:
         model = User
         fields = '__all__'
 
 
-class PaymentSerializer(serializers.ModelSerializer):
 
-    class Meta:
-        model = Payments
-        fields = '__all__'
