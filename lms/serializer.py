ea010f27ab2a36e0eb50955b6d89356002bde44e
Kovkov-D-S <158774590+Kovkov-D-S@users.noreply.github.com>
2025-03-08 00:51:18 +0300

    input field with information about lessons has been implemented for the serializer for the course model

--- lms/serializer.py
+++ lms/serializer.py
@@ -2,8 +2,16 @@ from rest_framework import serializers
 from lms.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     count_lessons = serializers.SerializerMethodField()
+    lesson_information = LessonSerializer(source='lesson_set', many=True)
 
     class Meta:
         model = Course
@@ -12,9 +20,3 @@ class CourseSerializer(serializers.ModelSerializer):
     def get_count_lessons(self, instance):
         return Lesson.objects.filter(course=instance).count()
 
-
-class LessonSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Lesson
-        fields = '__all__'
